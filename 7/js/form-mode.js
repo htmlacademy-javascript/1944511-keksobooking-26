51fdd8ca91785d0272068af12148597ead304ec1
Keks <keks@htmlacademy.ru>
2022-07-10 11:43:33 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form-mode.js
@@ -0,0 +1,31 @@
+const mapFiltersForm = document.querySelector('.map__filters');
+const adForm = document.querySelector('.ad-form');
+const adFormElements = adForm.children;
+const mapFiltersElements = mapFiltersForm.children;
+
+/** Функция изменяет состояние страницы - активное и неактивное
+ * @param {boolean} isActive — для неактивного состояния false, для активного true
+ */
+function changePageMode (isActive) {
+  if (!isActive) {
+    adForm.classList.add('ad-form--disabled');
+    for (const adFormElement of adFormElements) {
+      adFormElement.disabled = true;
+    }
+    mapFiltersForm.classList.add('map__filters--disabled');
+    for (const mapFiltersElement of mapFiltersElements) {
+      mapFiltersElement.disabled = true;
+    }
+  } else {
+    adForm.classList.remove('ad-form--disabled');
+    for (const adFormElement of adFormElements) {
+      adFormElement.disabled = false;
+    }
+    mapFiltersForm.classList.remove('map__filters--disabled');
+    for (const mapFiltersElement of mapFiltersElements) {
+      mapFiltersElement.disabled = false;
+    }
+  }
+}
+
+export { changePageMode };
---
+++ 7/js/main.js
@@ -0,0 +1,5 @@
+import './generation-adv.js';
+import { changePageMode } from './form-mode.js';
+import './form-validation.js';
+
+changePageMode(true);
